bbffa25d6721dbf6fbc4a01e9ea9325e3321b701
Abdikhaaliq Mohamed Aden <104009441+Abdiwali-Eid@users.noreply.github.com>
2024-03-28 14:51:16 +0300

    Update Dhex.jsx

--- src/components/Dhex.jsx
+++ src/components/Dhex.jsx
@@ -5,7 +5,7 @@ import Lemmon from '../assets/lemmon desert.jfif'
 import Brusta from '../assets/brustecha.jpg'
 import Avater from '../assets/avater.jfif'
 import dhex from '../assets/dhex.PNG'
-import dhex2 from '../assets/dhex2.PNG'
+
 
 const Dhex = () => {
   return (
@@ -31,7 +31,7 @@ served with a modern twist.
 </p>
     </div> 
 
-<img src={dhex2} alt="" style={{paddingLeft:'90px'}} />
+<img src="https://freepngimg.com/thumb/food/139197-food-plate-top-fish-view.png" alt="" style={{paddingLeft:'90px'}} />
 </div>
     </div>
   )
